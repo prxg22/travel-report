553ffa4d12dfee9fc2cc79cc95c04c7f8d6a3332
Paulo Ricardo Xavier Giusti <prxg22@gmail.com>
2019-05-12 02:17:37 -0300

    fix: fix report reference | chore: add circleci supprot

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const travel = require('./src/travel')
 const report = require('./src/report')
 const reference = require('firebase-reference-helper')
 
-module.exports = (admin, { ref, params } = {}) => {
+module.exports = (admin, { ref = '', params = {} }) => {
     if (!admin) throw Error('No admin provided!')
 
-    const rootRef = ref && reference(ref, params) || ''
+    const travelRef = ref && reference(ref, params) || ''
+    const aux = ref || '' + '/travles/{travel_id}'
+    const reportRef = reference(aux, { ...params, travel_id: '' })
 
     return {
-      Travel: travel(admin, rootRef),
-      Report: report(admin, rootRef),
+      Travel: travel(admin, travelRef),
+      Report: report(admin, reportRef),
     }
 }
